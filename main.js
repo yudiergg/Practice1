9c88b20ce9ac515cf7a5b35407c4d34aa797b5ba
yudiergg <gkscks1248@gmail.com>
2025-03-27 14:47:06 +0900

    tab구현할때 css로 default일때는 비활성화 active 클래스 추가하면 활성화되게
    ..

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $('.infor .tab > li').on('click', function(){
-        $('.infor .tab > li').removeClass('active')
-        $(this).addClass('active')
-    })
-})
+$(document).ready(function () {
+    $('.infor .tab > li').on('click', function () {
+        $('.infor .tab > li').removeClass('active').removeAttr('title'); // 모든 li에서 active 클래스 제거 및 title 삭제
+        $(this).addClass('active').attr('title', '선택됨'); // 클릭한 li에 active 클래스 추가 및 title 부여
+    });
+});
